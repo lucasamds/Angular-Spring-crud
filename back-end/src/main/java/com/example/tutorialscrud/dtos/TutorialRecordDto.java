c46f8e6880d4711990fc5b721715d3e03aa7669d
Lucas Silva <47309556+lucasamds@users.noreply.github.com>
2023-11-09 13:51:38 -0300

    Delete back-end directory

--- back-end/src/main/java/com/example/tutorialscrud/dtos/TutorialRecordDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.tutorialscrud.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record TutorialRecordDto(@NotBlank String title, @NotNull String description, @NotNull boolean published) {
-}
